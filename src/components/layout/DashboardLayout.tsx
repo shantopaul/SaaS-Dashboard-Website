a45e70f13be9237e677f4e7206fd59fc087badf9
Shanto Paul <shantopaul9944@gmail.com>
2026-05-19 23:11:10 +0600

    Fix Sidebar and Header scrolling by constraining DashboardLayout height

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -4,7 +4,7 @@ import { DashboardHeader } from "./DashboardHeader";
 
 export function DashboardLayout() {
   return (
-    <div className="flex min-h-screen w-full bg-muted/40">
+    <div className="flex h-screen w-full overflow-hidden bg-muted/40">
       {/* Skip-to-content link: visible on focus, hidden otherwise */}
       <a
         className="sr-only focus:not-sr-only focus:absolute focus:left-1/2 focus:top-4 focus:-translate-x-1/2 focus:z-[300] focus:block focus:rounded-lg focus:border focus:border-primary/20 focus:bg-background/95 focus:px-4 focus:py-2.5 focus:text-sm focus:font-semibold focus:text-primary focus:shadow-xl focus:backdrop-blur-md focus:outline-none focus:ring-2 focus:ring-primary"
@@ -14,8 +14,6 @@ export function DashboardLayout() {
       </a>
 
       <Sidebar />
-      {/* Desktop Sidebar Spacer */}
-      <div className="hidden w-64 shrink-0 md:block" />
 
       <div className="flex flex-1 flex-col min-w-0">
         <div className="flex flex-1 flex-col px-0 sm:px-4 md:px-6 sm:py-4">
